2dbded04d258908abe2f9408858807b46168b308
Husamoa <pawebial@gmail.com>
2018-11-24 16:26:19 +0100

    first steps

---
+++ Node-js-Udemy/notes-node/app.js
@@ -0,0 +1,20 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
+
+
+// console.log(_.isString(true));
+// console.log(_.isString('Pawel'));
+
+let filteredArray = _.uniq(['Pawel', 1 , 'Pawel', 1, 2, 3, 4])
+console.log(filteredArray);
+
+
+// let res = notes.add(9, -2);
+// console.log('Result: ' + res);
+// let user = os.userInfo();
+//
+// fs.appendFileSync('greenings.txt', `Hello ${user.username}! You are ${notes.age}.`);
